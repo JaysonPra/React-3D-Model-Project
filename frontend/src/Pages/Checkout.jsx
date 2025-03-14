1152909da243117e8c0eac9f5be47f8757d904b4
Aveg Byanjankar <avegbenz777@gmail.com>
2025-03-14 12:15:06 +0545

    UpdatedCheckout

--- frontend/src/Pages/Checkout.jsx
+++ frontend/src/Pages/Checkout.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <div className="text-2xl text-red-600 text-center m-10">
+      This is the checkout page.
+      <br />
+      Still Under Working!!!
+    </div>
+  );
 };
 
 export default Checkout;
